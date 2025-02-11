8969aacaf3f5c504c7f5cd510ad3ab73d2d649c0
scgm0 <2682963017@qq.com>
2025-02-11 13:30:19 +0800

    完善`关于`界面

--- MVL/Src/UI/Item/InfoItem.cs
+++ MVL/Src/UI/Item/InfoItem.cs
@@ -1,9 +1,39 @@
 using Godot;
+using MVL.Utils.Help;
 
 namespace MVL.UI.Item;
 
-public partial class InfoItem : PanelContainer {
+public partial class InfoItem : Control {
+	[Export]
+	private Label? _title;
+
+	[Export]
+	private RichTextLabel? _content;
+
+	public string Title {
+		get;
+		set {
+			field = value;
+			if (_title != null) {
+				_title.Text = value;
+			}
+		}
+	} = "";
+
+	public string Content {
+		get;
+		set {
+			field = value;
+			if (_content != null) {
+				_content.Text = value;
+			}
+		}
+	} = "";
+
 	public override void _Ready() {
-		
+		_title.NotNull();
+		_content.NotNull();
+		_title.Text = Title;
+		_content.Text = Content;
 	}
 }
--- MVL/Src/UI/Page/InfoPage.cs
+++ MVL/Src/UI/Page/InfoPage.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Godot;
+using MVL.UI.Item;
 using MVL.Utils.Help;
 
 namespace MVL.UI.Page;
@@ -14,6 +16,15 @@ public partial class InfoPage : MenuPage {
 	private Control? _titleBar;
 
 	[Export]
+	private Button? _authorButton;
+
+	[Export]
+	private Button? _donorButton;
+
+	[Export]
+	private Button? _licenseButton;
+
+	[Export]
 	private VBoxContainer? _list;
 
 	public override void _Ready() {
@@ -23,7 +34,36 @@ public partial class InfoPage : MenuPage {
 		_titleBar.NotNull();
 		_list.NotNull();
 		VisibilityChanged += OnVisibilityChanged;
+		_buttonGroup.Pressed += ButtonGroupOnPressed;
+		UpdateList(Info.AUTHORS);
+	}
+
+	private void ButtonGroupOnPressed(BaseButton button) {
+		if (button == _authorButton) {
+			UpdateList(Info.AUTHORS);
+		}
+
+		if (button == _donorButton) {
+			UpdateList(Info.DONORS);
+		}
+
+		if (button == _licenseButton) {
+			UpdateList(Info.LICENSES);
+		}
 	}
 
 	private void OnVisibilityChanged() { _titleBar!.Visible = !Visible; }
+
+	private void UpdateList(IReadOnlyList<(string, string)> items) {
+		foreach (var child in _list!.GetChildren()) {
+			child.QueueFree();
+		}
+
+		foreach (var valueTuple in items) {
+			var item = _itemScene!.Instantiate<InfoItem>();
+			item.Title = valueTuple.Item1;
+			item.Content = valueTuple.Item2;
+			_list.AddChild(item);
+		}
+	}
 }
