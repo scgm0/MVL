5460ef0313861bd63125047254edb0c79c4b6082
scgm0 <2682963017@qq.com>
2025-03-06 21:44:43 +0800

    优化单一启动器实例的实现

--- MVL/Src/UI/Start.cs
+++ MVL/Src/UI/Start.cs
@@ -4,25 +4,35 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using Environment = System.Environment;
+using FileAccess = System.IO.FileAccess;
 
 namespace MVL.UI;
 
 enum AppEventEnum { Focus }
 
 public partial class Start : Control {
+	static private FileStream? _lockFile;
 	static private TcpListener? _listener;
 
 	public Start() {
 		try {
-			_listener = new(IPAddress.Loopback, 35200);
+			_lockFile = File.Open(OS.GetTempDir().PathJoin("MVL-Lock"),
+				FileMode.OpenOrCreate,
+				FileAccess.ReadWrite,
+				FileShare.None);
+			_listener = new(IPAddress.Loopback, 0);
 			_listener.Start();
+			var port = ((IPEndPoint)_listener.LocalEndpoint).Port;
+			File.WriteAllTextAsync(OS.GetTempDir().PathJoin("MVL-Port"), port.ToString());
 			Task.Run(AppStartEventListen);
 		} catch (Exception) {
 			QueueFree();
 			Main.SceneTree.Root.MinSize = Vector2I.Zero;
 			Main.SceneTree.Root.Size = Vector2I.Zero;
+			var port = int.Parse(File.ReadAllText(OS.GetTempDir().PathJoin("MVL-Port")));
 			using var client = new TcpClient();
-			client.Connect(IPAddress.Loopback, 35200);
+			client.Connect(IPAddress.Loopback, port);
 			using var stream = client.GetStream();
 			using var writer = new StreamWriter(stream);
 			writer.Write(AppEventEnum.Focus);
