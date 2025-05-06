f25d5dc6296679ad8e04b1fe973e4ce36906aaa2
scgm0 <2682963017@qq.com>
2025-05-07 00:07:40 +0800

    [UI] 在BaseWindow中添加BackBufferCopy节点并关联导出变量

--- MVL/Src/UI/Window/BaseWindow.cs
+++ MVL/Src/UI/Window/BaseWindow.cs
@@ -19,6 +19,8 @@ public partial class BaseWindow : Control {
 
 	[Export] protected Button? OkButton;
 
+	[Export] protected BackBufferCopy? BackBufferCopy;
+
 	[Signal]
 	public delegate void CancelEventHandler();
 
