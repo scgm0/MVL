190a11718467315bcec8a0231437c7644bc405bf
scgm0 <2682963017@qq.com>
2025-07-21 04:34:09 +0800

    [UI] 优化作者输入框组件，新增背景模糊效果、输入完成自动清空功能及选中项点击处理逻辑

--- MVL/Src/UI/Other/AuthorsLineEdit.cs
+++ MVL/Src/UI/Other/AuthorsLineEdit.cs
@@ -1,16 +1,46 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using FuzzySharp;
+using Godot;
 using MVL.Utils.Game;
 
 namespace MVL.UI.Other;
 
 public partial class AuthorsLineEdit : CandidateLineEdit<ApiAuthor> {
-	public override IEnumerable<(ApiAuthor data, string candidate, int ratio)> GetCandidate() {
+
+	public override void _Ready() {
+		base._Ready();
+		Bg!.Hidden += BgOnHidden;
+	}
+
+	private void BgOnHidden() {
+		if (Selected is null) {
+			Text = "";
+		}
+	}
+
+	public override IEnumerable<(ApiAuthor data, int ratio)> GetCandidate() {
 		return Candidates.Select(data => {
 			var name = data.Name ?? data.UserId.ToString();
 			var ratio = Fuzz.Ratio(name, Text);
-			return (data, name, ratio);
+			return (data, ratio);
 		});
+	}
+
+	public override Button GetItemContainer((ApiAuthor data, int ratio) item) {
+		var button = new Button {
+			Text = item.data.Name,
+			TooltipText = $"{item.data.Name} ({item.data.UserId})",
+			Flat = true
+		};
+
+		button.Pressed += () => {
+			Selected = item.data;
+			Text = item.data.Name;
+			Bg?.Hide();
+		};
+
+		return button;
 	}
 }
