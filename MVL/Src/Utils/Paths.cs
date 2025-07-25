a58f4b2b1f6836222c4450453c9ee58b513f804d
scgm0 <2682963017@qq.com>
2025-07-26 04:26:24 +0800

    [工具类] 新增缓存文件夹路径 CacheFolder 并在 EnsureFolderExists 方法中确保其目录存在

--- MVL/Src/Utils/Paths.cs
+++ MVL/Src/Utils/Paths.cs
@@ -7,17 +7,22 @@ public static class Paths {
 	public static string BaseConfigPath { get; } = OS.GetUserDataDir().PathJoin("data.json").NormalizePath();
 	public static string ModpackFolder { get; } = OS.GetUserDataDir().PathJoin("Modpack").NormalizePath();
 	public static string ReleaseFolder { get; } = OS.GetUserDataDir().PathJoin("Release").NormalizePath();
+	public static string CacheFolder { get; } = OS.GetUserDataDir().PathJoin(".Cache").NormalizePath();
 	public static string LockFile { get; } = OS.GetTempDir().PathJoin("MVL-Lock").NormalizePath();
 	public static string PortFile { get; } = OS.GetTempDir().PathJoin("MVL-Port").NormalizePath();
 	static Paths() { EnsureFolderExists(); }
 
-	public static void EnsureFolderExists() {
+	static private void EnsureFolderExists() {
 		if (!DirAccess.DirExistsAbsolute(ModpackFolder)) {
 			DirAccess.MakeDirRecursiveAbsolute(ModpackFolder);
 		}
 
 		if (!DirAccess.DirExistsAbsolute(ReleaseFolder)) {
 			DirAccess.MakeDirRecursiveAbsolute(ReleaseFolder);
+		}
+
+		if (!DirAccess.DirExistsAbsolute(CacheFolder)) {
+			DirAccess.MakeDirRecursiveAbsolute(CacheFolder);
 		}
 	}
 }
