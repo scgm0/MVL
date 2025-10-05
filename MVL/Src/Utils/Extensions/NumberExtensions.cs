bac4ada0e377a0abca21d6e2f718d50b31746e5f
scgm0 <2682963017@qq.com>
2025-10-05 22:19:11 +0800

    [代码优化] 将NumberExtensions.FormatNumber方法重构为新的扩展语法

--- MVL/Src/Utils/Extensions/NumberExtensions.cs
+++ MVL/Src/Utils/Extensions/NumberExtensions.cs
@@ -3,30 +3,31 @@ namespace MVL.Utils.Extensions;
 public static class NumberExtensions {
 	static private readonly string[] Suffixes = ["", "k", "M", "G", "T", "P"];
 
-	public static string FormatNumber(this int number) {
-		if (number == 0) {
-			return "0";
+	extension(int number) {
+		public string FormatNumber() {
+			if (number == 0) {
+				return "0";
+			}
+
+			var isNegative = number < 0;
+			var absValue = isNegative ? (long)-number : number;
+
+			double value = absValue;
+			var unitIndex = 0;
+
+			while (value >= 1000.0 && unitIndex < Suffixes.Length - 1) {
+				value /= 1000.0;
+				unitIndex++;
+			}
+
+			if (value >= 999.95 && unitIndex < Suffixes.Length - 1) {
+				value /= 1000.0;
+				unitIndex++;
+			}
+
+			var formattedValue = value.ToString("0.#");
+
+			return (isNegative ? "-" : "") + formattedValue + Suffixes[unitIndex];
 		}
-
-		var isNegative = number < 0;
-		var absValue = isNegative ? (long)-number : number;
-
-		double value = absValue;
-		var unitIndex = 0;
-
-		while (value >= 1000.0 && unitIndex < Suffixes.Length - 1) {
-			value /= 1000.0;
-			unitIndex++;
-		}
-
-		if (value >= 999.95 && unitIndex < Suffixes.Length - 1) {
-			value /= 1000.0;
-			unitIndex++;
-		}
-
-		var formattedValue = value.ToString("0.#");
-
-		return (isNegative ? "-" : "") + formattedValue + Suffixes[unitIndex];
 	}
-
 }
